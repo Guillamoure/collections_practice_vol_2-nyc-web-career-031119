190163ea22f8dfa941e5ac8e3fe52a7f559d5f9e
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-02-27 18:38:37 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -70,7 +70,7 @@ def merge_data(keys, data)
   ashley_array = [keys[1]]
   data.each do |name|
     if name.values == ashley
-      print (name.values)
+      print data.name.values
     end
     if name.values == blake
       blake_array.push(name.values)

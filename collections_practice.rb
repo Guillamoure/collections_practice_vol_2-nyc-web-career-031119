3cfb061cbd31b0add60609a3582cedfe83b6e7b4
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-02-27 18:27:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,72 @@
 # your code goes here
+def begins_with_r(array)
+  j = []
+  array.each do |word|
+    if word[0] == "r" || word[0] == "R"
+      j << "r"
+    end
+  end
+  if j.length == array.length
+    return TRUE
+  else
+    return FALSE
+  end
+end
+
+def contain_a(array)
+  j = []
+  array.each do |word|
+    if word.include? "a"
+      j << word
+    end
+  end
+    return j
+end
+
+def first_wa(array)
+  array.collect do |word|
+    if word.class != String
+      strung = word.id2name
+    else
+      strung = word
+    end
+    if strung.include? "wa"
+      return strung
+    end
+  end
+end
+
+def remove_non_strings(array)
+  j = []
+  array.each do |word|
+    if word.class == String
+      j << word
+    end
+  end
+  return j
+end
+
+def count_elements(array)
+  j = []
+  coles_problem = {}
+  array.each do |hashbrown|
+    a = hashbrown.values
+    if (coles_problem.has_value?(a[0]) == FALSE)
+       n = array.count(hashbrown)
+       coles_problem = hashbrown
+      coles_problem[:count] = n
+      j << coles_problem
+    else
+    end
+  end
+  return j
+end
+
+def merge_data(keys, data)
+  i = 0
+  print blake = keys[0][:first_name]
+  print ashley = keys[1][:first_name]
+  data.each do |name|
+    
+  end
+end
